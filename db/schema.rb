9c92b5d0fda754c00d7555315547494bd5d906ac
Hiroshi Kajisha <kajisha@gmail.com>
2020-10-01 15:16:01 +0900

    Add Company model which is a subclass of Party

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_061425) do
+ActiveRecord::Schema.define(version: 2020_10_01_061522) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "name", null: false
